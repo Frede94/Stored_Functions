e4acd916f4f7fbde5f5e230074bf7f64679cbd21
Frede94 <32297979+Frede94@users.noreply.github.com>
2022-03-11 17:11:32 +0100

    Update usp_DeleteDepartment.sql

--- usp_DeleteDepartment.sql
+++ usp_DeleteDepartment.sql
@@ -4,6 +4,14 @@ Create PROCEDURE usp_DeleteDepartment
 )
 AS
 BEGIN
-	DELETE FROM Department Where DNumber = @DNumber
+	DECLARE @Pno int;
+			SELECT @Pno (SELECT PNumber FROM Project WHERE DNum = @DNumber)
+	
+	UPDATE Employee SET Dno = NULL
+	DELETE FROM Department WHERE Department.DNumber = @DNumber
+	DELETE FROM Dept_Locations WHERE Dept_Locations.DNUmber = @DNumber
+	DELETE FROM Project WHERE Project.DNum = @DNumber
+	DELETE FROM Works_on WHERE Works_on.Pno = @Pno
+	
 	RETURN SCOPE_IDENTITY();
 END
