957cc1c9313f8c1656e486eb334a3aa212ae7b85
Frede94 <32297979+Frede94@users.noreply.github.com>
2022-03-11 12:04:13 +0100

    Update usp_CreateDepartment.sql

--- usp_CreateDepartment.sql
+++ usp_CreateDepartment.sql
@@ -1,8 +1,7 @@
 Create PROCEDURE usp_CreateDepartment
 (
 	@DName NVARCHAR(50),
-	@MgrSSN INT,
-	@Dnumber INT
+	@MgrSSN INT
 	--@MgrStartDate DATE
 )
 AS
@@ -11,9 +10,11 @@ BEGIN
 		THROW 50001, 'DName already in use', 1 --First 50000 already in use 
 	IF EXISTS (SELECT 1 FROM Department WHERE MgrSSN = @MgrSSN)
 		THROW 50002, 'MgrSSN is already a Mgr', 1 --First 50000 already in use 
-	select @Dnumber=count(Dnumber)+1 from Department
-	INSERT INTO Department(DName,MgrSSN, MgrStartDate, Dnumber)
-		VALUES (@DName, @MgrSSN, GetDate())
+
+	DECLARE @DNumber int;
+		SELECT @DNumber = coalesce((select max(DNumber) + 1 from Department), 1)
+		INSERT INTO Department(DName, DNumber, MgrSSN, MgrStartDate)
+			VALUES (@DName, @DNumber, @MgrSSN, GetDate())
 
 	RETURN SCOPE_IDENTITY(); -- Returns Identity from this Scope.
 	--RETURN @@IDENTITY -- Returns latest Identity, Could be from another function/Procedure.
