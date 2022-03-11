50683568a6a048c8215be73d634fbbdfa429f2cc
Frede94 <32297979+Frede94@users.noreply.github.com>
2022-03-11 11:51:14 +0100

    Update usp_CreateDepartment.sql

--- usp_CreateDepartment.sql
+++ usp_CreateDepartment.sql
@@ -1,7 +1,8 @@
 Create PROCEDURE usp_CreateDepartment
 (
 	@DName NVARCHAR(50),
-	@MgrSSN INT
+	@MgrSSN INT,
+	@Dnumber INT
 	--@MgrStartDate DATE
 )
 AS
@@ -10,7 +11,8 @@ BEGIN
 		THROW 50001, 'DName already in use', 1 --First 50000 already in use 
 	IF EXISTS (SELECT 1 FROM Department WHERE MgrSSN = @MgrSSN)
 		THROW 50002, 'MgrSSN is already a Mgr', 1 --First 50000 already in use 
-	INSERT INTO Department(DName,MgrSSN, MgrStartDate)
+	select @Dnumber=count(Dnumber)+1 from Department
+	INSERT INTO Department(DName,MgrSSN, MgrStartDate, Dnumber)
 		VALUES (@DName, @MgrSSN, GetDate())
 
 	RETURN SCOPE_IDENTITY(); -- Returns Identity from this Scope.
