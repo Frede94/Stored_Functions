259d4cab730598cba979c9009bb4da2e89f150fa
Frede94 <32297979+Frede94@users.noreply.github.com>
2022-03-11 20:48:03 +0100

    Update usp_UpdateDepartmentName.sql

--- usp_UpdateDepartmentName.sql
+++ usp_UpdateDepartmentName.sql
@@ -1,1 +1,14 @@
-execute ups
+create PROCEDURE usp_UpdateDepartmentName
+(
+	@DName NVARCHAR(50),
+	@DNumber INT
+)
+AS
+BEGIN
+	IF EXISTS (SELECT DName From Department Where DName = @DName)
+		THROW 50006,'Name is already in use by another Department.',1;
+	ELSE
+		UPDATE Department
+		SET DName = @DName
+		WHERE DNumber = @DNumber
+END
